d7c72681ba75222ff3fc67a2eadf538c9f600167
Alexgf22 <agarcia@iesrafaelalberti.es>
2022-12-28 13:01:55 +0100

    creando metodos add, pop, tope, toString

--- src/main/kotlin/Pila.kt
+++ src/main/kotlin/Pila.kt
@@ -1,2 +1,24 @@
-class Pila {
+class Pila<T> {
+    // Lista mutable de elementos de cualquier tipo
+    private val elementos = mutableListOf<T>()
+
+    // devuelve el tamano de la lista
+    private fun sizeList() : Int = this.elementos.size
+
+
+    // devuelve el elemento en la cabeza de la pila.
+    fun tope(): T = this.elementos.first()
+
+
+    // inserta en la lista el elemento que se le pasa como parametro
+    fun push(elemento : T) {
+        this.elementos.add(elemento)
+    }
+
+
+    // devuelve el ultimo elemento de la lista
+    fun pop() : T = this.elementos.removeAt(this.elementos.lastIndex)
+
+
+    override fun toString() = this.elementos.toString()
 }
