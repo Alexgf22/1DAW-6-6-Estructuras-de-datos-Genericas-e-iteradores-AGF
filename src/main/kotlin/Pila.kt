8f99d3abebd21f8cc073cc7a103a4f407ed57c40
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-01-11 21:46:46 +0100

    documentando la clase y los metodos

--- src/main/kotlin/Pila.kt
+++ src/main/kotlin/Pila.kt
@@ -1,37 +1,82 @@
+/**
+ * La clase Pila generica le pongo de Tipo T queriendo decir de que puede ser
+ * de cualquier tipo.
+ *
+ * @property elementos MutableList<T>  Diversos datos que puede contener la lista
+ * que pueden ser de nuevo de cualquier tipo.
+ */
 class Pila<T>(
     private val elementos: MutableList<T> = mutableListOf()
 ) : Iterador<T> {
 
 
-    // devuelve el tamano de la lista
+
+    /**
+     * Esta funcion calcula el tamano de la lista.
+     *
+     * @return Int devuelve el numero de elementos de la lista.
+     */
     private fun sizeList() : Int = this.elementos.size
 
 
-    // devuelve el elemento en la cabeza de la pila.
+
+    /**
+     * La funcion lo que hace es llamar al metodo first() de una lista mutable
+     * para saber cual es el elemento que se encuentra primero en la lista.
+     *
+     * @return T? devuelve el elemento que se encuentra en la cabeza de la pila
+     * que puede ser de cualquier tipo.
+     */
     fun tope(): T? = this.elementos.first()
 
 
-    // inserta en la lista el elemento que se le pasa como parametro
+
+    /**
+     * La funcion lo que hace es insertar en la lista mutable un elemento que puede
+     * ser de cualquier tipo.
+     *
+     * @param elemento que puede ser de cualquier tipo.
+     */
     fun push(elemento : T) {
         this.elementos.add(elemento)
     }
 
 
-    // devuelve el ultimo elemento de la lista
+
+    /**
+     * La funcion lo que hace es sacar de la lista el elemento que se encuentra
+     * en el ultimo indice.
+     *
+     * @return T devuelve el ultimo elemento de la lista.
+     */
     fun pop() : T = this.elementos.removeAt(this.elementos.lastIndex)
 
 
     /* si esta vacia (tope es nulo, o tamano es 0) o
        llena (si existe tamano maximo)
     */
+    /**
+     * La funcion comprueba llamando a la funcion sizeList() si el tamano de la
+     * lista es 0 o si la funcion tope() es nula .
+     *
+     * @return Boolean devuelve true si se cumple alguna de las 2 condiciones
+     * mencionadas anteriormente. En caso contrario, devuelve false.
+     */
     fun vacia(): Boolean {
         return sizeList() == 0 || tope() == null
     }
 
-    /*override fun recorrido(elementos: MutableList<T>) {
+    /*override fun iterando(elementos: MutableList<T>) {
         super.recorrido(elementos)
     }*/
-
+    /**
+     * La funcion crea una copia de la lista para ir eliminando los elementos
+     * de ahi y crea otra lista donde se iran introduciendo los elementos.
+     * Despues va recorriendo los indices de lista y en cada uno mete en la lista
+     * del reves en ese mismo indice el ultimo elemento que haya en la lista copia.
+     *
+     * @return MutableList<T> devuelve la lista del reves.
+     */
     fun reverse(): MutableList<T> {
         val listaMod = elementos.size -1
         val copiaLista = elementos
